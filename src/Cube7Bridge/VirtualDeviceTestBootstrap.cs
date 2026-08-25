5dd2d2dfde13de5bff9aeed4d31da78e9d3ca713
FuzzsT <218664044+FuzzsT@users.noreply.github.com>
2026-08-25 18:19:19 +0200

    test: execute virtual device tests in self-test mode

---
+++ src/Cube7Bridge/VirtualDeviceTestBootstrap.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+
+namespace Cube7Bridge;
+
+internal static class VirtualDeviceTestBootstrap
+{
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        if (Environment.GetCommandLineArgs().Any(a => string.Equals(a, "--self-test", StringComparison.OrdinalIgnoreCase)))
+            VirtualDeviceEmulationTests.Run();
+    }
+}
